cc74c212d474e6000765a0e2c0160db9a65ade53
MohamedGEDI <gedimohammed@gmail.com>
2021-11-26 13:34:02 +0300

    read me

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -6,7 +6,7 @@ import hashlib
 import uuid
 
 
-class User():
+class User:
     """
     User class:
     - id: public string unique (uuid)
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -52,9 +52,10 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        if self.__password is None:
+        elif self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        elif hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password:
+            return True
 
 
 if __name__ == '__main__':
@@ -84,7 +85,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
+    if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
